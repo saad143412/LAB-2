ffbc87d10d1375d518cd1cd02ce34c0e9cf50093
saad143412 <88199414+saad143412@users.noreply.github.com>
2021-08-01 18:29:15 +0500

    Add files via upload

---
+++ LAB2 Area of Rectangle.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	/*Programmer Name:Muhammad Saad
+	  Reg.No:20MDELE163            */
+  	 
+	printf("Programmer Name:Muhammad Saad\nReg.No:20MDELE163\n\n");
+	
+	int L,B,Area;
+	printf("please enter Length and Breadth of a Rectangle: ");
+	scanf("%d%d",&L,&B);
+	Area=L*B;
+	printf("The Required Area of Rectangle is:%d",Area);
+	
+	
+}
+
+
---
+++ LAB2 Sum of Two numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	/*Programmer Name:Muhammad Saad
+	  Reg.No:20MDELE163            */
+  	 
+	printf("Programmer Name:Muhammad Saad\nReg.No:20MDELE163\n\n");
+	
+	int num1,num2,sum;
+	printf("please enter two numbers:");
+	scanf("%d%d",&num1,&num2);
+	sum=num1+num2;
+	printf("The Sum of %d and %d is:%d",num1,num2,sum);
+	
+	
+}
+
+
